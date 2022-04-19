575316386a6d7a149784295e60ab3e3cfa07abe4
CanhNguyen2k2 <99537619+CanhNguyen2k2@users.noreply.github.com>
2022-04-19 15:47:08 +0700

    Add files via upload

---
+++ BaiThucHanh1904/BaiThucHanh1904/src/Tiger.java
@@ -0,0 +1,17 @@
+public class Tiger extends Animal {
+    public void Eat(){
+        System.out.println("Eating...!");
+    }
+    public void Sleep(){
+        System.out.println("Sleeping...!");
+    }
+    public void Walk(){
+        System.out.println("Walking...!");
+    }
+    public void Run(){
+        System.out.println("Running...!");
+    }
+    public void Roar(){
+        System.out.println("ROARRR!");
+    }
+}
